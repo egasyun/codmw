c7427011519aaddc815302867cc1ceeca63a39ca
江頭俊介 <shunsuke@egashirashunsukenoMacBook-Air.local>
2019-10-28 17:59:52 +0900

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,31 @@
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+lock "3.9.0"
+
+set :application, "withFriend" #自分のapp_nameに変更
+set :repo_url, "git@example.com:me/my_repo.git" #自分のリポジトリURLに変更
+set :deploy_to, '/var/www/withFriend' #自分のapp_nameに変更
+set :keep_releases, 3
+
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.4.1'
+
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
+set :linked_dirs, %w{log tmp/backup tmp/pids tmp/cache tmp/sockets vendor/bundle}
+set :linked_files, %w{config/secrets.yml}
+
+set :bundle_jobs, 4
+
+namespace :deploy do
+  task :restart do
+    on roles(:web, :app, :db) do |host|
+      execute "source /etc/environment"
+    end
+    invoke 'unicorn:restart'
+  end
+  after :finishing, 'deploy:cleanup'
+end
